5ff4046242ca89917815179624059285b9a21523
Doran Kayoumi <dorankayoumi@gmail.com>
2019-10-30 14:51:49 +0100

    Implement Teacher class

--- src/school/Teacher.java
+++ src/school/Teacher.java
@@ -1,4 +1,24 @@
 package school;
 
-class Teacher {
+public class Teacher extends Person {
+    private String abbreviation;
+
+    public Teacher(String fname, String lname, String abbr) {
+        super(fname, lname);
+
+        this.abbreviation = abbr;
+    }
+
+    public String abbreviation() {
+        return abbreviation;
+    }
+
+    public String schedule() {
+        return "";
+    }
+
+    @Override
+    public String toString() {
+        return "Teach. " + super.toString() + " (" + this.abbreviation + ")";
+    }
 }
