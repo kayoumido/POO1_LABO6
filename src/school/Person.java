d6e0f7c8ed358bdc5cc5a9b4576136a9c6c14018
Doran Kayoumi <dorankayoumi@gmail.com>
2019-10-30 14:50:54 +0100

    Implement Persion class

--- src/school/Person.java
+++ src/school/Person.java
@@ -1,4 +1,16 @@
 package school;
 
 class Person {
+    private String firstName;
+    private String lastName;
+
+    Person(String fname, String lname) {
+        this.firstName  = fname;
+        this.lastName   = lname;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 }
