2e23f1715e1f7fa53973f4a64c35a9d64de72b61
Doran Kayoumi <dorankayoumi@gmail.com>
2019-10-30 14:51:21 +0100

    Implement Student class

--- src/school/Student.java
+++ src/school/Student.java
@@ -1,4 +1,17 @@
 package school;
 
-class Student {
+public class Student extends Person {
+
+    private int idNumber;
+
+    public Student(String fname, String lname, int id) {
+        super(fname, lname);
+
+        this.idNumber = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Stud. " + super.toString() + " (#" + this.idNumber + ")";
+    }
 }
