8c2cbca3bce7f64d881627a5523f174fc8f30a9a
Doran Kayoumi <dorankayoumi@gmail.com>
2019-10-30 14:52:07 +0100

    create test objects

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+
+import school.Student;
+import school.Teacher;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Teacher pier = new Teacher("Pier", "Donini", "PDO");
+        Teacher marc = new Teacher("Marc", "Dikötter", "MDK");
+
+        Student john    = new Student("John", "Lennon", 1234);
+        Student paul    = new Student("Paul", "Mc Cartney", 2341);
+        Student ringo   = new Student("Ringo", "Starr", 3412);
+        Student george  = new Student("George", "Harisson", 4321);
+        Student roger   = new Student("Roger", "Waters", 1324);
+        Student david   = new Student("David", "Gilmour", 4312);
     }
 }
