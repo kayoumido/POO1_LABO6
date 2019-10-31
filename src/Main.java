f2d8e6aa2e75b2c61a1fb1dbb4aa34722637c522
Doran Kayoumi <dorankayoumi@gmail.com>
2019-10-31 15:36:20 +0100

    Create objects

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
+import school.Group;
 import school.Student;
 import school.Teacher;
+import school.Lesson;
+
+import java.util.ArrayList;
 
 public class Main {
 
@@ -14,5 +18,22 @@ public class Main {
         Student george  = new Student("George", "Harisson", 4321);
         Student roger   = new Student("Roger", "Waters", 1324);
         Student david   = new Student("David", "Gilmour", 4312);
+
+        ArrayList<Student> il = new ArrayList<>();
+        il.add(john);
+        il.add(paul);
+        il.add(ringo);
+        il.add(george);
+
+        ArrayList<Student> ts = new ArrayList<>();
+        ts.add(roger);
+        ts.add(david);
+
+        Group il61 = new Group(6, "IL", 1, il);
+        Group ts61 = new Group(6, "TS", 1, ts);
+
+        Lesson poo1 = new Lesson(pier, "POO1", 2, 3, 90, "G02");
+        Lesson pls  = new Lesson(marc, "PLP", 3, 3, 135, "H01");
+
     }
 }
