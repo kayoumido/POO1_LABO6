19811a7ec88c30563ebf29599b0ad3eb4dc60b5e
Doran Kayoumi <dorankayoumi@gmail.com>
2019-10-31 15:21:03 +0100

    Add group attribut

--- src/school/Student.java
+++ src/school/Student.java
@@ -3,15 +3,27 @@ package school;
 public class Student extends Person {
 
     private int idNumber;
+    private Group group;
 
-    public Student(String fname, String lname, int id) {
+    public Student(String fname, String lname, int id, Group group) {
         super(fname, lname);
 
         this.idNumber = id;
+        this.group = group;
+    }
+
+    public Student(String fname, String lname, int id) {
+        this(fname, lname, id, null);
+    }
+
+    void setGroup(Group group) {
+        this.group = group;
     }
 
     @Override
     public String toString() {
-        return "Stud. " + super.toString() + " (#" + this.idNumber + ")";
+        return "Stud. " + super.toString() + " (#" + this.idNumber + ")" + (
+                this.group != null ? " - " + this.group.name() : ""
+        );
     }
 }
