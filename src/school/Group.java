c70131d90a4a851d8cfa649c1529289470b0eee7
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2019-10-31 15:17:00 +0100

    Add group and lesson

--- src/school/Group.java
+++ src/school/Group.java
@@ -1,4 +1,41 @@
 package school;
 
-class Group {
+import java.util.ArrayList;
+
+public class Group {
+
+    private int numero;
+    private String orientation;
+    private int trimester;
+    private ArrayList<Student> students;
+
+
+    public Group(int numero, String orientation, int trimester, ArrayList<Student> students){
+        // Check weekday validity
+        if(trimester < 1 || trimester > 6){
+            throw new IllegalArgumentException("The week day must be between 0 (monday) and 6 (sunday).");
+        }
+
+        this.numero = numero;
+        this.orientation = orientation;
+        this.trimester = trimester;
+        this.students = students;
+    }
+
+
+    public String name () {
+        return orientation + numero + "-" + trimester ;
+    }
+
+    public int nombreEtudiants() {
+        return students.size();
+    }
+
+    public void definirLecon() {
+
+    }
+
+    public String horaire(){
+        return "";
+    }
 }
--- src/school/Lesson.java
+++ src/school/Lesson.java
@@ -1,4 +1,36 @@
 package school;
 
 class Lesson {
+
+    private Teacher teacher;
+    private String subject;
+    private int weekDay;
+    private int periodBeginning;
+    private int duration;
+    private String classroom;
+
+
+    public Lesson(Teacher teacher, String subject, int weekDay, int periodBeginning, int duration, String classroom) {
+        this(subject, weekDay, periodBeginning, duration, classroom);
+        this.teacher = teacher;
+    }
+
+    private Lesson(String subject, int weekDay, int periodBeginning, int duration, String classroom) {
+
+        // Check weekday validity
+        if(weekDay < 0 || weekDay > 6){
+            throw new IllegalArgumentException("The week day must be between 0 (monday) and 6 (sunday).");
+        }
+
+        this.subject = subject;
+        this.weekDay = weekDay;
+        this.periodBeginning = periodBeginning;
+        this.duration = duration;
+        this.classroom = classroom;
+    }
+
+    public static String horaire(){
+        return "Horaire";
+    }
+
 }
